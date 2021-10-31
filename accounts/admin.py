a240fb0d6d8d06b5f1a863660e90a8843fe44cce
Fiverr2555 <92688251+Fiverr2555@users.noreply.github.com>
2021-10-31 23:01:18 +0500

    Delete accounts directory

--- accounts/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Block,userBlock
-# Register your models here.
-
-admin.site.register(Block)
-admin.site.register(userBlock)
--- accounts/templates/test.py
+++
@@ -1,7 +0,0 @@
-count = 0
-for i in range (1,26):
-    print('<tr height="40px" style="border:3px white solid">')
-    for j in range(1,26):
-        count += 1 
-        print('<td height="40px" width="40px" style="border:3px white solid">'+str(count)+'</td>')
-    print('</tr>')
